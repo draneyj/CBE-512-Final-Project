3c21d38582f90da1c39aba69345ad0a7e0fb4067
draneyjosu <draneyj@oregonstate.edu>
2021-12-08 23:21:19 -0500

    made getThermoData functional

--- getThermoData.py
+++ getThermoData.py
@@ -1,17 +1,34 @@
 from scrape import makeURL
 from random import random
+from time import sleep
+from requests import get
+import numpy as np
 import pandas as pd
 
-def getStateDF(ID,P):
+def getStateDf(ID,P):
     page = get(makeURL(P, ID))
     sleep(random() + 0.25)
     tables = pd.read_html(page.text)
     Ptable = tables[0]
     return Ptable
+def getManyStates(molrow,N=300):
+    ID = molrow['ID']
+    Prange = molrow['Prange']
+    Ps = np.linspace(Prange[0], Prange[1], N)
+    smallDfs = []
+    for P in Ps:
+        smallDfs.append(getStateDf(ID,P))
+    return pd.concat(smallDfs)
+def addMolRows(molrow,singleDf):
+    for column in molrow.keys():
+        singleDf[column] = [molrow[column]] * len(singleDf)
+    return singleDf
 
 if __name__ == "__main__":
-    pass
-# generate uniform distribution of 200 Ps from a compound
-# make dataframe with info from mols in first columns and each row of PVT table
-# repeat for all IDs
-# save to data.pkl
+    mols = pd.read_pickle('mols.pkl')
+    smallDfs = []
+    for i, mol in mols.iterrows():
+        smallDf = getManyStates(mol, 300)
+        smallDfs.append(addMolRows(mol, smallDf))
+    bigDf = pd.concat(smallDfs)
+    pd.to_pickle(bigDf,'data.pkl')
